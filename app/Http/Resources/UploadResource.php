34388cea5119735d2a631873990d243a6badc192
Chu Phong <chucamphong1999@gmail.com>
2020-05-08 14:39:30 +0700

    Sửa lỗi không tìm thấy url

--- app/Http/Resources/UploadResource.php
+++ app/Http/Resources/UploadResource.php
@@ -4,16 +4,13 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-/**
- * @property string $url
- */
 class UploadResource extends JsonResource
 {
     public function toArray($request): array
     {
         return [
             'base' => asset(\Storage::url(null)),
-            'path' => $this->url
+            'path' => $this['url']
         ];
     }
 }
