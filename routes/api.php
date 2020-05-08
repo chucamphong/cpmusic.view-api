ff8dac0cbf12e090f2e2f2daa5625fd19c2eb140
Chu Phong <chucamphong1999@gmail.com>
2020-05-08 16:41:19 +0700

    Xóa route thừa

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ Auth::routes();
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('users', 'UserController');
-    Route::post('users/avatar', 'UploadController@userAvatar')->name('users.avatar.store');
 
     Route::apiResource('songs', 'SongController');
 
