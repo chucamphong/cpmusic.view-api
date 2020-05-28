e2aaaea838d6830534f39fa896d741c6bd8ead39
Chu Phong <chucamphong1999@gmail.com>
2020-05-28 19:26:07 +0700

    Refactor: Tối ưu code
    - Sử dụng component x-song thay thế

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,22 +36,7 @@
             <a href="#xem-them" class="h5 mb-0">Xem thêm</a>
         </div>
         @foreach($topSongs as $key => $song)
-            <div class="col-6 col-sm-4 col-md-4 col-lg-2">
-                <div class="card bg-transparent shadow-none">
-                    <a href="{{ route('song.listen', $song->id) }}" title="{{ $song->name }}">
-                        <img src="{{ Storage::url($song->thumbnail) }}" alt="{{ $song->name }}"
-                             class="card-img-top img-fluid">
-                    </a>
-                    <div class="card-body px-0 py-1">
-                        <a href="{{ route('song.listen', $song->id) }}" title="{{ $song->name }}">
-                            <h4 class="text-truncate text-capitalize m-0">{{$song->name}}</h4>
-                        </a>
-                        <h5 class="text-muted text-truncate">{!! $song->artists->map(function ($artist) {
-                            return "<a href='/ca-si/$artist->id'>$artist->name</a>";
-                        })->join(', ') !!}</h5>
-                    </div>
-                </div>
-            </div>
+            <x-song :song="$song"></x-song>
         @endforeach
     </div>
 
@@ -66,22 +51,7 @@
             /** @var \App\Models\Artist $artist */
         @endphp
         @foreach($songs as $key => $song)
-            <div class="col-6 col-sm-4 col-md-4 col-lg-2">
-                <div class="card bg-transparent shadow-none">
-                    <a href="{{ route('song.listen', $song->id) }}" title="{{ $song->name }}">
-                        <img src="{{ Storage::url($song->thumbnail) }}" alt="{{ $song->name }}"
-                             class="card-img-top img-fluid">
-                    </a>
-                    <div class="card-body px-0 py-1">
-                        <a href="{{ route('song.listen', $song->id) }}" title="{{ $song->name }}">
-                            <h4 class="text-truncate text-capitalize m-0">{{$song->name}}</h4>
-                        </a>
-                        <h5 class="text-muted text-truncate">{!! $song->artists->map(function ($artist) {
-                            return "<a href='/ca-si/$artist->id'>$artist->name</a>";
-                        })->join(', ') !!}</h5>
-                    </div>
-                </div>
-            </div>
+            <x-song :song="$song"></x-song>
         @endforeach
     </div>
 
