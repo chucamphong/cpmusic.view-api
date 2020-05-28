e5f8dd6d11b4ad42aa00158b63269cdc1282bd8d
Chu Phong <chucamphong1999@gmail.com>
2020-05-28 21:57:11 +0700

    Fix: Tối ưu truy vấn

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -16,7 +16,9 @@ class SongController extends Controller
     public function listen(int $id): Renderable
     {
         /** @var Song|Builder $song */
-        $song = Song::with('artists:name')->findOrFail($id);
+        $song = Song::with('artists:name')
+            ->select('id', 'name', 'thumbnail', 'url', 'views', 'category_id')
+            ->findOrFail($id);
 
         $song->increment('views', 1);
 
@@ -26,9 +28,7 @@ class SongController extends Controller
                 return $query->whereIn('id', $song['artists']->pluck('pivot.artist_id'));
             })
             // Hoặc các bài hát có cùng thể loại
-            ->orWhereHas('category', function (Builder $query) use ($song) {
-                return $query->where('id', $song->category_id);
-            })
+            ->orWhere('category_id', $song->category_id)
             // Lấy ngẫu nhiên 6 bài hát
             ->inRandomOrder()->limit(6)->get();
 
