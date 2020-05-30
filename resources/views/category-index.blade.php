ba23e9b1e4ba185f19e0121919ee4502181e1902
ChuPhong <chucamphong1999@gmail.com>
2020-05-30 13:02:31 +0700

    Fix: Chỉnh sửa giao diện

--- resources/views/category-index.blade.php
+++ resources/views/category-index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="row">
-    <div class="col-12 d-flex align-items-center justify-content-between mb-2">
-        <h2 class="d-inline mb-0 text-uppercase">Thể loại</h2>
+    <div class="col-12 d-flex align-items-center justify-content-between mb-3">
+        <h1 class="mb-0 ls-1 text-uppercase">Thể loại</h1>
     </div>
     @foreach($categories as $category)
         <div class="col-6 col-sm-4 col-xl-2 {{ $loop->index >= 4 ? 'd-none d-sm-block' : null }}">
