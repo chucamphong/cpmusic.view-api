49c4e8b8e000a447a05d3c67328a4e2938748d55
ChuPhong <chucamphong1999@gmail.com>
2020-05-30 13:00:06 +0700

    Fix: Hiển thị ảnh đại diện của nghệ sĩ

--- resources/views/artist.blade.php
+++ resources/views/artist.blade.php
@@ -4,13 +4,17 @@
     /** @var \App\Models\Artist $artist */
 @endphp
 
+@section('title', "Nghệ sĩ $artist->name")
+
 @section('content')
     <div class="row">
-        <div class="col-12 mb-4">
-            <h1 class="display-3 mb-0 ls-1">{{ $artist->name }}</h1>
-            <span class="h5 text-uppercase ls-1 text-muted">Hiện có</span>
-            <span class="h4 text-muted">{{ $artist->songs_count }}</span>
-            <span class="h5 text-uppercase ls-1 text-muted">bài hát</span>
+        <div class="col-12 d-flex align-items-center mb-4">
+            <img src="{{ Storage::url($artist->avatar) }}" alt="{{ $artist->name }}" class="rounded-circle"
+                 style="max-width: 64px"/>
+            <div class="d-flex flex-column ml-3">
+                <h1 class="mb-0 ls-1">{{ $artist->name }}</h1>
+                <span class="h5 text-uppercase ls-1 text-muted">Hiện có {{ $artist->songs_count }} bài hát</span>
+            </div>
         </div>
 
         @foreach ($songs as $song)
