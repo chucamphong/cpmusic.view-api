7b4a31f20eb65065d86acdd0b5be5c055b15bf3c
ChuPhong <chucamphong1999@gmail.com>
2020-05-30 09:32:56 +0700

    Fix: Cập nhật giao diện

--- public/js/app.js
+++ public/js/app.js
@@ -115,8 +115,8 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /mnt/e/projects/api.cpmusic/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /mnt/e/projects/api.cpmusic/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/cpmusic/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/cpmusic/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/layouts/partials/sidenav.blade.php
+++ resources/views/layouts/partials/sidenav.blade.php
@@ -21,10 +21,38 @@
                 <!-- Nav items -->
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link {{ Request::is(route('home')) ?: 'active' }}" href="{{ route('home') }}">
+                        <a class="nav-link {{ !Route::is('home') ?: 'active' }}" href="{{ route('home') }}">
                             <i class="ni ni-shop text-primary"></i>
                             <span class="nav-link-text">Trang Chủ</span>
                         </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ !Route::is('song.most-viewed') ?: 'active' }}" href="#navbar-song-charts" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-song-charts">
+                            <i class="ni ni-ui-04 text-info"></i>
+                            <span class="nav-link-text">Bảng Xếp Hạng</span>
+                        </a>
+                        <div class="collapse" id="navbar-song-charts">
+                            <ul class="nav nav-sm flex-column">
+                                <li class="nav-item">
+                                    <a href="{{ route('song.most-viewed', ['country' => 'Việt Nam']) }}" class="nav-link">
+                                        <span class="sidenav-mini-icon">V</span>
+                                        <span class="sidenav-normal">Việt Nam</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('song.most-viewed', ['country' => 'US-UK']) }}" class="nav-link">
+                                        <span class="sidenav-mini-icon">U</span>
+                                        <span class="sidenav-normal">US-UK</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('song.most-viewed', ['country' => 'Hàn Quốc']) }}" class="nav-link">
+                                        <span class="sidenav-mini-icon">Hàn Quốc</span>
+                                        <span class="sidenav-normal">Hàn Quốc</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </li>
                 </ul>
                 <!-- Divider -->
