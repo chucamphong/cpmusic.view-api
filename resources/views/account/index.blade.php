e7ac154a78da647e74a38e7237b3dc3887e1ff0e
ChuPhong <chucamphong1999@gmail.com>
2020-05-31 13:13:01 +0700

    Rephrase: Thêm comment và tối ưu

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -22,64 +22,49 @@
                     <form method="post" action="{{ route('account.update') }}" enctype="multipart/form-data">
                         @csrf
 
+                        <!-- Tên hiển thị -->
                         <div class="form-group row">
                             <label for="name" class="col-md-3 col-form-label text-md-right">Tên hiển thị</label>
 
                             <div class="col-md-9">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
-                                       name="name" value="{{ $user->name }}" autocomplete="name" autofocus>
+                                       name="name" value="{{ $user->name }}" autocomplete="name" autofocus/>
 
                                 @error('name')
-                                <span class="invalid-feedback" role="alert">
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
+                        <!-- Địa chỉ email -->
                         <div class="form-group row">
                             <label for="email" class="col-md-3 col-form-label text-md-right">Địa chỉ Email</label>
 
                             <div class="col-md-9">
-                                <input id="email" disabled type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user->email }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                       value="{{ $user->email }}" required disabled />
 
                                 @error('email')
-                                <span class="invalid-feedback" role="alert">
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
-{{--                        <div class="form-group row">--}}
-{{--                            <label for="password" class="col-md-3 col-form-label text-md-right">Mật khẩu</label>--}}
-
-{{--                            <div class="col-md-9">--}}
-{{--                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">--}}
-
-{{--                                @error('password')--}}
-{{--                                <span class="invalid-feedback" role="alert">--}}
-{{--                                        <strong>{{ $message }}</strong>--}}
-{{--                                    </span>--}}
-{{--                                @enderror--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="form-group row">--}}
-{{--                            <label for="password-confirm" class="col-md-3 col-form-label text-md-right">Nhập lại mật khẩu</label>--}}
-
-{{--                            <div class="col-md-9">--}}
-{{--                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
+                        <!-- Ảnh đại diện -->
                         <div class="form-group row">
                             <label for="avatar" class="col-md-3 col-form-label text-md-right">Ảnh đại diện</label>
 
                             <div class="col-md-9">
                                 <div class="custom-file">
-                                    <input name="avatar" type="file" class="custom-file-input @error('avatar') is-invalid @enderror" id="avatar" lang="en" accept="image/png, image/jpeg" />
+                                    <input name="avatar" type="file"
+                                           class="custom-file-input @error('avatar') is-invalid @enderror" id="avatar"
+                                           lang="en" accept="image/png, image/jpeg"/>
                                     <label class="custom-file-label" for="avatar"></label>
+
                                     @error('avatar')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
